98404e3decaf99180d52e35b37c2e0d65e0c95de
ryosuke.mega <ryosuke.mega@gmail.com>
2020-05-14 12:32:29 +0000

    for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_063936) do
+ActiveRecord::Schema.define(version: 2020_05_09_085310) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
     t.string "status"
   end
 
