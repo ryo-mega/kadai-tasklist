83eb6fb1ab84c90fa85517711ba0f65a65f11b66
ryosuke.mega <ryosuke.mega@gmail.com>
2020-05-21 13:43:25 +0000

    user signup2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  root to: 'toppages#index'
-
+  root to: 'tasks#index'
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :create]
+  resources :tasks
 end
