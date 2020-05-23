2bbec0b9484c90833d7f0f76de4d2cc2a06ad85c
ryosuke.mega <ryosuke.mega@gmail.com>
2020-05-23 03:04:19 +0000

    post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_16_091057) do
+ActiveRecord::Schema.define(version: 2020_05_23_025407) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
