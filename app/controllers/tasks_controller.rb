3581788c6487bb871511e35007e313a7928bb861
ryosuke.mega <ryosuke.mega@gmail.com>
2020-06-03 12:32:56 +0000

    version4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:destroy, :show]
+  before_action :correct_user, only: [:destroy, :show, :update]
   def index
     #if logged_in?
       #@task = current_user.tasks.build  # form_with 用
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
-  get 'sessions/destroy'
   root to: 'tasks#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:create]
   resources :tasks
 end
